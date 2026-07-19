a6a4491375448f6b2a7a4ff6b9b3cbfce1513546
vladislavyevsikov <vladislav.yevsikov@beetweb.com>
2026-07-19 22:24:32 +0300

    Add esp setup

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,17 @@ hw_timer_t* timer = nullptr;
 
 volatile bool secondTick = false;
 uint32_t seconds = 0;
+
+void IRAM_ATTR onTimer() {
+    secondTick = true;
+}
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+
+    timer = timerBegin(1000000);
+    timerAttachInterrupt(timer, &onTimer);
+    timerAlarm(timer, 1000000, true, 0);
+}
+
